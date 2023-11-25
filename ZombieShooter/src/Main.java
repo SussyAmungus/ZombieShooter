dff684c277d8194219c033ec17de626e8937d17d
SussyAmungus <104276104+SussyAmungus@users.noreply.github.com>
2023-11-25 11:46:28 -0700

    new line

--- ZombieShooter/src/Main.java
+++ ZombieShooter/src/Main.java
@@ -4,7 +4,9 @@ public class Main {
 	public static void main(String args[]) {
 		
 		System.out.println("Alibaba");
-		sadasdasda
+
+
+		System.out.println("New line lmao111111111111111111");
 		
 	}
 	
